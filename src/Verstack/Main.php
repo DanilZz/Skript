8cbd7fc03bf286d041eb37fe5110eb1595c58a29
Daniil Link <thegame200000@gmail.com>
2019-08-02 02:28:08 +0700

    Update Main.php

--- src/Verstack/Main.php
+++ src/Verstack/Main.php
@@ -3,18 +3,36 @@
 namespace Verstack;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
+//use pocketmine\event\Listener;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase{
 	
 	public $skripts = [];
 	
+	public $handler;
+	
 	public function onEnable(){
 		@mkdir($this->getDataFolder());
+		$this->handler = new Handler($this);
+		$this->getServer()->getPluginManager()->registerEvents(new Listener($this), $this);
 		$this->getServer()->getLogger()->info("[Skript] Загрузка скриптов...");
 		foreach(glob($this->getDataFolder()."*.sk") as $file){
 			$this->skripts[] = new Skript($this, $file);
 		}
+	}
+	
+	public function event($type, $p){
+		foreach($this->skripts as $sk){
+			if(isset($sk->data[$type])){
+				foreach($sk->data[$type] as $line){
+					$this->getHandler()->handleLine($line, $p);
+				}
+			}
+		}
+	}
+	
+	public function getHandler(){
+		return $this->handler;
 	}
 	
 }
