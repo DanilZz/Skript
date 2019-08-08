8107b23d4d6ad9936b00026af983cb15ee2e8ae7
Daniil Link <thegame200000@gmail.com>
2019-08-08 18:32:10 +0700

    Update Skript.php

--- src/Verstack/Skript.php
+++ src/Verstack/Skript.php
@@ -12,18 +12,25 @@ class Skript{
 	
 	public $data;
 	
+	public $handler;
+	
 	public function __construct($main, $file){
 		$this->main = $main;
 		$this->file = $file;
 		$this->data = (new Config($file, Config::YAML))->getAll();
+		$this->handler = new Handler($this);
 		$this->onEnable();
 	}
 	
 	public function onEnable(){
 		$this->getServer()->getLogger()->info("[Skript] Скрипт ".basename($this->file)." загружен.");
 		foreach($this->data as $type => $data){
-			$this->main->getHandler()->handleFunc($type, $data);
+			$this->getHandler()->handleFunc($type, $data);
 		}
+	}
+	
+	public function getHandler(){
+		return $this->handler;
 	}
 	
 	public function getServer(){
